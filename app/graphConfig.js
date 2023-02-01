faa7c9b12dd30d72ed5116fee2ffb3b6dcc86042
takedamasaki555 <masakit@microsoft.com>
2023-02-01 18:27:56 +0900

    Update graphConfig.js

--- app/graphConfig.js
+++ app/graphConfig.js
@@ -1,5 +1,5 @@
 // Add here the endpoints for MS Graph API services you would like to use.
 const graphConfig = {
     graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
-    API2Endpoint: "https://backend-2.azurewebsites.net/api/httptrigger1"
+    API2Endpoint: "https://apim555.azure-api.net/functionapp/api/httptrigger1" //"https://backend-2.azurewebsites.net/api/httptrigger1"
 };
